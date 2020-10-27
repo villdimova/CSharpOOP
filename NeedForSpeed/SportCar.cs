ffb5dad325b2f305fd48d9888c1edbb117ad58c6
villdimova <70020646+villdimova@users.noreply.github.com>
2020-10-27 18:25:19 +0200

    Add files via upload

---
+++ NeedForSpeed/SportCar.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace NeedForSpeed
+{
+    public class SportCar : Car
+    {
+        private const double defaultFuelConsumption = 10;
+        public SportCar(int horsePower, double fuel) : base(horsePower, fuel)
+        {
+           
+        }
+
+        public override double FuelConsumption => defaultFuelConsumption;
+       
+    }
+}
