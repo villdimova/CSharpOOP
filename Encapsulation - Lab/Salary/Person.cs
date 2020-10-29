9cf1ef78ddf3029c428399a625c6ab6912c3686e
villdimova <70020646+villdimova@users.noreply.github.com>
2020-10-29 19:34:47 +0200

    Add files via upload

---
+++ Encapsulation - Lab/Salary/Person.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonsInfo
+{
+   public class Person
+    {
+        public Person(string firstName, string lastName, int age,decimal salary)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+            this.Salary = salary;
+
+        }
+
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public int Age { get; private set; }
+        public decimal Salary { get; private set; }
+
+        public void IncreaseSalary(decimal percentage)
+        {
+            decimal increasePercentage = percentage / 100;
+            if (this.Age<30)
+            {
+                increasePercentage /= 2;
+            }
+
+            this.Salary += Salary * increasePercentage;
+        }
+        public override string ToString()
+        {
+            return ($"{this.FirstName} {this.LastName} receives {this.Salary:f2} leva.");
+
+        }
+    }
+}
