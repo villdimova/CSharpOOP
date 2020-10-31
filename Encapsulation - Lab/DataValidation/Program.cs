9ed3310af15b8b48f6e22da2e732ada187842c77
villdimova <70020646+villdimova@users.noreply.github.com>
2020-10-31 20:01:09 +0200

    Add files via upload

---
+++ Encapsulation - Lab/DataValidation/Program.cs
@@ -0,0 +1,39 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace PersonsInfo
+{
+   public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var lines = int.Parse(Console.ReadLine());
+            var persons = new List<Person>();
+            for (int i = 0; i < lines; i++)
+            {
+                var command = Console.ReadLine().Split();
+                var firstName = command[0];
+                var lastName = command[1];
+                var age = int.Parse(command[2]);
+                var salary = decimal.Parse(command[3]);
+
+                try
+                {
+                    Person person = new Person(firstName,lastName, age, salary);
+                    persons.Add(person);
+
+                }
+                catch (Exception ex)
+                {
+
+                    Console.WriteLine(ex);
+                }
+            }
+
+            var parcentage = decimal.Parse(Console.ReadLine());
+            persons.ForEach(p => p.IncreaseSalary(parcentage));
+            persons.ForEach(p => Console.WriteLine(p.ToString()));
+        }
+    }
+}
---
+++ Encapsulation - Lab/Team/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PersonsInfo
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var lines = int.Parse(Console.ReadLine());
+            var persons = new List<Person>();
+
+            
+            for (int i = 0; i < lines; i++)
+            {
+                var command = Console.ReadLine().Split();
+                var firstName = command[0];
+                var lastName = command[1];
+                var age = int.Parse(command[2]);
+                var salary = decimal.Parse(command[3]);
+                var person = new Person(firstName, lastName, age, salary);
+                persons.Add(person);
+            }
+            Team team = new Team("SoftUni");
+            foreach (Person person in persons)
+            {
+                team.AddPlayer(person);
+            }
+
+            Console.WriteLine($"First team has {team.FirstTeam.Count} players.");
+            Console.WriteLine($"Reserve team has {team.ReserveTeam.Count} players.");
+        }
+    }
+}
