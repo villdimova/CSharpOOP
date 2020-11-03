bd54c768400713c06d68395825a87591d0b65c4b
villdimova <70020646+villdimova@users.noreply.github.com>
2020-11-03 18:43:04 +0200

    Add files via upload

---
+++ InterfacesAndAbstraction/Shapes/IDrawable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public interface IDrawable
+    {
+
+        public void Draw();
+        
+    }
+}
