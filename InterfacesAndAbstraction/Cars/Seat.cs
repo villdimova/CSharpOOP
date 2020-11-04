a83cacde4ca5eab988870670a55722732e112486
villdimova <70020646+villdimova@users.noreply.github.com>
2020-11-04 10:19:28 +0200

    Add files via upload

---
+++ InterfacesAndAbstraction/Cars/Seat.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    public class Seat:ICar
+    {
+        public string Model { get; }
+        public string Color { get; }
+
+        public Seat(string model, string color)
+        {
+            this.Model = model;
+            this.Color = color;
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{this.Color} Seat {this.Model} ");
+            sb.AppendLine(Start());
+            sb.AppendLine(Stop());
+            return sb.ToString().TrimEnd();
+        }
+
+        public string Start()
+        {
+            return "Engine start";
+        }
+
+        public string Stop()
+        {
+            return "Breaaak!";
+        }
+    }
+}
---
+++ InterfacesAndAbstraction/Cars/Tesla.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    public class Tesla : ICar, IElectricCar
+    {
+        public string Model { get; }
+        public string Color { get; }
+        public int Batteries { get; }
+
+        public Tesla(string model, string color, int batteries)
+        {
+            this.Model = model;
+            this.Color = color;
+            this.Batteries = batteries;
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{this.Color} Tesla {this.Model} with {this.Batteries} Batteries");
+            sb.AppendLine(Start());
+            sb.AppendLine(Stop());
+            return sb.ToString().TrimEnd();
+        }
+
+        public string Start()
+        {
+            return "Engine start";
+        }
+
+        public string Stop()
+        {
+            return "Breaaak!";
+        }
+    }
+}
