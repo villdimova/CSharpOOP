f34a6b9ab3e90aebdba537b13c3185b1f45dd209
villdimova <70020646+villdimova@users.noreply.github.com>
2020-11-05 20:21:25 +0200

    Add files via upload

---
+++ InterfacesAndAbstraction-Exercices/FoodShortage/Classes/Citizen.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FoodShortage
+{
+    public class Citizen : IIdentifiable, IBirthable, IBuyer
+    {
+       
+
+        public Citizen(string name, int age, string id, string birthdate)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Id = id;
+            this.Birtdate = birthdate;
+            this.Food = 0;
+        }
+
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Birtdate { get; set; }
+        public int Food { get; set; }
+
+        public int BuyFood()
+        {
+           return  this.Food += 10;
+        }
+    }
+}
+
+
---
+++ InterfacesAndAbstraction-Exercices/FoodShortage/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FoodShortage
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            var persons = new List<IBuyer>();
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                var personInfo = Console.ReadLine().Split().ToArray();
+                if (personInfo.Length==4)
+                {
+                    Citizen citizen = new Citizen(personInfo[0], int.Parse(personInfo[1]), personInfo[2], personInfo[3]);
+                    persons.Add(citizen);
+                }
+                else if (personInfo.Length==3)
+                {
+                    Rebel rebel = new Rebel(personInfo[0], int.Parse(personInfo[1]), personInfo[2]);
+                    persons.Add(rebel);
+                }
+
+            }
+
+          
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if(input== "End")
+                {
+                    break;
+                }
+                else
+                {
+                    foreach (var person in persons)
+                    {
+                        if (person.Name==input)
+                        {
+                            person.BuyFood();
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine(persons.Sum(p=>p.Food));
+            
+        }
+    }
+}
