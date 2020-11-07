3953393acef30319a0de07c85e880a918c2be1a2
villdimova <70020646+villdimova@users.noreply.github.com>
2020-11-07 21:22:48 +0200

    Add files via upload

---
+++ Polymorphism-Lab/Shapes/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Shapes
+{
+   public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Shape circle = new Circle(5);
+            Shape rectangle = new Rectangle(4, 3);
+
+            List<Shape> shapes = new List<Shape>();
+            shapes.Add(circle);
+            shapes.Add(rectangle);
+            foreach (var shape in shapes)
+            {
+                Console.WriteLine(shape.CalculateArea());
+                Console.WriteLine(shape.CalculatePerimeter());
+                Console.WriteLine(shape.Draw());
+            }
+        }
+    }
+}
