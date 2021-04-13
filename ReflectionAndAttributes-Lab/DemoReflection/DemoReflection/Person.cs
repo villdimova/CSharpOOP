f49a3694cf48f5ef41a2d647e5e180bce5456061
Velyana Dimova <70020646+villdimova@users.noreply.github.com>
2021-04-13 12:18:11 +0300

    Add files via upload

---
+++ ReflectionAndAttributes-Lab/DemoReflection/DemoReflection/Person.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoReflection
+{
+    public class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+        private string country;
+
+
+       
+        public Person(string firstName, string lastName, int age, string country)
+        {
+            this.FirstName = firstName;
+            this.LastName = LastName;
+            this.Age = age;
+            this.Country = Country;
+        }
+
+
+        protected Person()
+        {
+
+        }
+        public string FirstName
+        {
+            get { return this.firstName; }
+           private  set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Not valid value");
+                }
+                this.firstName = value;
+            }
+        }
+        public string LastName
+        {
+            get { return this.lastName; }
+           private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Not valid value");
+                }
+                this.lastName = value;
+            }
+        }
+        public int Age
+        {
+            get { return this.age; }
+            private set
+            {
+                if (this.Age<=0)
+                {
+                    throw new ArgumentException("Not valid value");
+                }
+                this.age = value;
+            }
+        }
+        public string Country
+        {
+            get { return this.country; }
+          private  set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Not valid value");
+                }
+                this.country = value;
+            }
+        }
+
+
+
+        public  string WhoAmI()
+        {
+            return ($"I am {this.FirstName} {this.LastName} {this.Age} age old from {this.Country}");
+        }
+    }
+}
---
+++ ReflectionAndAttributes-Lab/DemoReflection/DemoReflection/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Reflection;
+
+namespace DemoReflection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Type personType= typeof(Person);
+
+            FieldInfo privateField = personType.GetField("age", BindingFlags.NonPublic | BindingFlags.Instance);
+            Console.WriteLine(privateField.ToString());
+
+            FieldInfo[] privateFields = personType.GetFields(BindingFlags.NonPublic | BindingFlags.Instance);
+            foreach (var field in privateFields)
+            {
+                Console.WriteLine($"{field.ToString()}");
+            }
+
+            MethodInfo whoAmI = personType.GetMethod("WhoAmI", BindingFlags.Public | BindingFlags.Instance);
+
+
+            Console.WriteLine(whoAmI.ToString());
+
+            MethodInfo[] methods = personType.GetMethods();
+
+            foreach (var method in methods)
+            {
+                Console.WriteLine(method.ToString());
+            }
+
+            ConstructorInfo constructorInfo = personType.GetConstructor(new[] { typeof(string), typeof(string), typeof(int), typeof(string) });
+
+            Console.WriteLine(constructorInfo);
+
+            ConstructorInfo protectedCtorInfo = personType.GetConstructor(BindingFlags.NonPublic | BindingFlags.Instance,
+
+                null,
+                new Type[] { },
+                null);
+
+            Console.WriteLine(protectedCtorInfo);
+
+        }
+    }
+}
